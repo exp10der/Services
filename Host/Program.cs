f37b45eb4704c5311a17fd5f187d77a1aa22505f
Konstantin <konstantin.writing.code@gmail.com>
2017-01-05 02:38:34 +0300

    (new) Configure HostFactory

--- Host/Program.cs
+++ Host/Program.cs
@@ -11,9 +11,16 @@
             {
                 cfg.Service<EndpointService>(s =>
                 {
+                    s.ConstructUsing(() => new EndpointService());
                     s.WhenStarted(es => es.Start());
                     s.WhenStopped(es => es.Stop());
                 });
+
+                cfg.RunAsLocalSystem();
+
+                cfg.SetDescription("Endpoint service");      
+                cfg.SetDisplayName("Endpoint");                  
+                cfg.SetServiceName("Endpoint");                  
             });
         }
     }
